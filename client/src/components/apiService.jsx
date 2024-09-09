451f2a5fb3c1d9d300f2ef548a8a77f120e08f5c
aom081 <matemsom@gmail.com>
2024-09-09 10:58:13 +0700

    update 5

--- client/src/components/apiService.jsx
+++ client/src/components/apiService.jsx
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const API_URL = "https://dicedreams-backend-deploy-to-render.onrender.com/api/postGame";
+const API_URL = "https://dicedreams-backend-deploy-to-render.onrender.com/api/postGame/get_postGame_search";
 
 export const getPostGames = async (status) => {
   try {
-    // Adding status filter as a query parameter
     const response = await axios.get(API_URL, {
-      params: { status_post: status },
+      params: { status_post: status }, // Add the status parameter
     });
     return response.data;
   } catch (error) {
